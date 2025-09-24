1d283345ce61a20ebe88d8024072da5118abbec6
Alegenio2 <78229681+Alegenio2@users.noreply.github.com>
2025-09-24 10:54:03 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -13,4 +13,21 @@ const swiper = new Swiper('.swiper', {
   hamburger.addEventListener('click', () => {
     nav.classList.toggle('active');
   });
-  
+
+  const campeonesSwiper = new Swiper('.campeones-swiper', {
+  slidesPerView: 1,
+  spaceBetween: 15,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    480: { slidesPerView: 2 },
+    768: { slidesPerView: 3 },
+  },
+});
